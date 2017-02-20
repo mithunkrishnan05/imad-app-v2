34f36b3dc88c83318d18cfd4b2f1a8c50a8af976
mithunkrishnan05 <mithunkrishnan05@gmail.com>
2017-02-20 12:51:41 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,9 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/article-one',function (req,res){
+    res.senfile(path.join(_dirname,'ui','article-one'));
+});
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
