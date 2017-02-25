4b92c8fa88c3c4e662ab00a729fca7613d3ac9c9
mithunkrishnan05 <mithunkrishnan05@gmail.com>
2017-02-25 10:01:45 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,12 +1,9 @@
 console.log('Loaded!');
-alert('hii');
-
 var element = document.getElementById('hi');
 element.innerHTML='new value';
-
 var img = document.getElementById('madi');
 img.onclick=function () {
-    img.style.marginleft='100px';
+img.style.marginleft='100px';
 };
 
 
