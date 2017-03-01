17059a43cde188ca85dd0dfe7d2dccb225b50ede
mithunkrishnan05 <mithunkrishnan05@gmail.com>
2017-03-01 11:30:04 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(morgan('combined'));
 var counter=0;
 app.get('/counter',function (req, res){
     counter=counter+1;
-    res.sendFile(counter.tostring());
+    res.sendFile(counter.toString());
 });
 
 
