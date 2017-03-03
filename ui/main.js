9a65d45c18ab883af35d2b014e82c6ac6616e913
mithunkrishnan05 <mithunkrishnan05@gmail.com>
2017-03-03 09:26:00 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,11 +1,11 @@
 //counter code
 var counter=0;
-var button=document.getElementById('counter');
+var button=document.getElementById("counter");
 
 
 button.onclick = function () {
     counter=counter+1;
-    var span=document.getElementById('count');
+    var span=document.getElementById("count");
     span.innerHTML=counter.toString();
 };
 
